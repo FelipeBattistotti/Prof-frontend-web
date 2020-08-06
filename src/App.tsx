53a8fcb4374f72490469e205f4dc3bea16b4ec22
felipebattistotti <feligatti@gmail.com>
2020-08-06 02:22:31 -0300

    Commit frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import Landing from './pages/Landing';
-
 import './assets/styles/global.css';
+import Routes from './routes';
 
 // JSX - JavaScript + XML
 function App() {
   return (
-    <Landing />
+    <Routes />
   );
 }
 
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                
+            </header>
+        </div>
+    )
+}
+
+export default TeacherList;
