91144e5543ad19d94f6050dbcde6b57c7ed1e25c
felipebattistotti <feligatti@gmail.com>
2020-08-09 15:51:04 -0300

    Commit frontend web

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -26,33 +25,11 @@ function TeacherList() {
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars3.githubusercontent.com/u/61482397?s=460&u=07693e36331296afe32930fd5a758d4a72d9c4ed&v=4" alt="Felipe Battistotti"/>
-                        <div>
-                            <strong>Felipe Battistotti</strong>
-                            <span>Matemática</span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br/><br/>
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
-
-                    <footer>
-                        <p>
-                            Preço/Hora
-                            <strong>R$ 100,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp"/>
-                            Entrar em Contato
-                        </button>
-                    </footer>
-
-                </article>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
             </main>
         </div>
     );
