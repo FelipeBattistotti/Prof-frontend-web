8aa938b58ef6ac2e4d749a6de4093b4184d9725e
felipebattistotti <feligatti@gmail.com>
2020-08-20 23:59:46 -0300

    Commit frontend

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -11,14 +11,14 @@ function TeacherItem() {
                 <img src="https://avatars3.githubusercontent.com/u/61482397?s=460&u=07693e36331296afe32930fd5a758d4a72d9c4ed&v=4" alt="Felipe Battistotti"/>
                 <div>
                     <strong>Felipe Battistotti</strong>
-                    <span>Matemática</span>
+                    <span>Algoritmos</span>
                 </div>
             </header>
 
             <p>
-                Entusiasta das melhores tecnologias de química avançada.
+                Entusiasta das melhores tecnologias da Computação.
                 <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                Apaixonado por programação.
             </p>
 
             <footer>
