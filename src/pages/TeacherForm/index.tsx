acb37813e315a881b6c6a33ad105776f6069fedb
felipebattistotti <feligatti@gmail.com>
2020-08-27 23:59:44 -0300

    Commit frontend

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input'
+import Textarea from '../../components/Textarea'
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
@@ -21,6 +22,8 @@ function TeacherForm() {
                     <Input name="name" label="Nome Completo" />
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="WhatsApp" />
+
+                    <Textarea name="bio" label="Biografia" />
                 </fieldset>
 
                 <fieldset>
