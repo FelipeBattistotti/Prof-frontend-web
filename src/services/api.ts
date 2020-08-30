103d13b553ee360072924cff4411cd7cae7bc424
felipebattistotti <feligatti@gmail.com>
2020-08-30 18:38:05 -0300

    Commit frontend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
 
-//import dotenv from 'dotenv';
-
-//dotenv.config();
-
 const api = axios.create({
-    baseURL: 'http://localhost:5003',
+    baseURL: process.env.REACT_APP_API_URL,
 });
-
-// baseURL: process.env.REACT_APP_API_URL,
 
 export default api;
