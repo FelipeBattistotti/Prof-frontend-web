703cd51ba5c13aaf53cac67fd0954525d6cb6203
felipebattistotti <feligatti@gmail.com>
2020-08-31 23:13:35 -0300

    Commit frontend web

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,39 +4,39 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-interface TeacherItemProps {
-    teacher: {
-        id: string;
-        name: string;
-        avatar: string;
-        bio: string;
-        cost: number;
-        subject: string;
-        whatsapp: string;
-    };
+export interface Teacher {
+    id: string;
+    name: string;
+    avatar: string;
+    bio: string;
+    cost: number;
+    subject: string;
+    whatsapp: string;
 }
 
-const TeacherItem: React.FC<TeacherItemProps> = () => {
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/61482397?s=460&u=07693e36331296afe32930fd5a758d4a72d9c4ed&v=4" alt="Felipe Battistotti"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Felipe Battistotti</strong>
-                    <span>Algoritmos</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Entusiasta das melhores tecnologias da Computação.
-                <br /><br />
-                Apaixonado por programação.
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 100,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
                 <button type="button">
                     <img src={whatsappIcon} alt="Whatsapp"/>
